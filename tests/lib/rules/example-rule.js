41a9aa2d7cc3f89b7ce93cdc7c81f4fbc699ec7a
Toru Kobayashi <koba0004@gmail.com>
2019-06-09 00:36:35 +0900

    feat: add a rule for deprecated goog.json.serialize

--- tests/lib/rules/example-rule.js
+++ tests/lib/rules/example-rule.js
@@ -5,12 +5,14 @@ const rule = require("../../../lib/rules/example-rule");
 
 new RuleTester().run("example-rule", rule, {
     valid: [
-        "var foo = 1;"
+        "var foo = goog.array.serialize(data);",
+        "var foo = goog.json.hoge(data);"
     ],
     invalid: [
         {
-            code: "var example = 1;",
-            errors: ["'example' identifier is forbidden."]
+            code: "var contents = goog.json.serialize(data);",
+            output: "var contents = JSON.parse(data);",
+            errors: ["goog.json.serialize is deprecated. Please use JSON.parse instead."]
         }
     ]
 });
