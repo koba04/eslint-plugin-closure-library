1e25862063079b7985a050428722b3899c72ea03
Toru Kobayashi <koba0004@gmail.com>
2019-06-20 13:55:11 +0900

    fix: support ArrayLike object with prefer-native-array-methods rule

--- tests/lib/rules/prefer-native-array-methods.js
+++ tests/lib/rules/prefer-native-array-methods.js
@@ -8,27 +8,29 @@ new RuleTester().run("prefer-native-apis", rule, {
   invalid: [
     {
       code: "goog.array.forEach(list, function(el) { return el })",
-      output: "list.forEach(function(el) { return el })",
+      output: "Array.prototype.forEach.call(list, function(el) { return el })",
       errors: ["use Array.prototype.forEach instead of goog.array.forEach."]
     },
     {
       code: "goog.array.forEach(this.foo, function(el) { return el })",
-      output: "this.foo.forEach(function(el) { return el })",
+      output:
+        "Array.prototype.forEach.call(this.foo, function(el) { return el })",
       errors: ["use Array.prototype.forEach instead of goog.array.forEach."]
     },
     {
       code: "goog.array.forEach(this.foo, function(el) { return el }, ctx)",
-      output: "this.foo.forEach(function(el) { return el }, ctx)",
+      output:
+        "Array.prototype.forEach.call(this.foo, function(el) { return el }, ctx)",
       errors: ["use Array.prototype.forEach instead of goog.array.forEach."]
     },
     {
       code: "goog.array.map(list, function(el) { return el })",
-      output: "list.map(function(el) { return el })",
+      output: "Array.prototype.map.call(list, function(el) { return el })",
       errors: ["use Array.prototype.map instead of goog.array.map."]
     },
     {
       code: "goog.array.some(list, function(el) { return el })",
-      output: "list.some(function(el) { return el })",
+      output: "Array.prototype.some.call(list, function(el) { return el })",
       errors: ["use Array.prototype.some instead of goog.array.some."]
     }
   ]
